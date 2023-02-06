8b96e3642e272c0192767310bc862ecd4d037fbd
unknown <deliah07@naver.com>
2023-02-06 11:58:18 +0900

    글쓰기 기능 완료

--- src/main/java/shop/mtcoding/blog/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -1,11 +1,27 @@
 package shop.mtcoding.blog.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import shop.mtcoding.blog.dto.user.BoardReq.BoardSaveReqDto;
+import shop.mtcoding.blog.handler.ex.CustomException;
+import shop.mtcoding.blog.model.User;
+import shop.mtcoding.blog.service.BoardService;
 
 @Controller
 public class BoardController {
+
+    @Autowired
+    private HttpSession session;
+
+    @Autowired
+    private BoardService boardService;
 
     @GetMapping({ "/", "/board" })
     public String main() {
@@ -20,6 +36,29 @@ public class BoardController {
     @GetMapping("/board/saveForm")
     public String saveForm() {
         return "board/saveForm";
+    }
+
+    // title이랑 content를 받는
+    @PostMapping("/board")
+    public String save(BoardSaveReqDto boardSaveReqDto) {
+        User principal = (User) session.getAttribute("principal");
+        if (principal == null) {
+            throw new CustomException("인증이 되지 않았습니다", HttpStatus.UNAUTHORIZED);
+
+        }
+        if (boardSaveReqDto.getTitle() == null || boardSaveReqDto.getTitle().isEmpty()) {
+            throw new CustomException("title을 작성해주세요");
+        }
+        if (boardSaveReqDto.getContent() == null || boardSaveReqDto.getContent().isEmpty()) {
+            throw new CustomException("content를 작성해주세요");
+        }
+
+        // int result =
+        boardService.글쓰기(boardSaveReqDto);
+        // if (result != 1) {
+        // throw new CustomException("회원가입실패")
+        // }
+        return "redirect:/";
     }
 
     @GetMapping("/board/{id}/updateForm")
--- src/main/java/shop/mtcoding/blog/handler/CustomExceptionHandler.java
+++ src/main/java/shop/mtcoding/blog/handler/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.blog.handler;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -7,11 +8,12 @@ import shop.mtcoding.blog.handler.ex.CustomException;
 import shop.mtcoding.blog.util.Script;
 
 @RestControllerAdvice
-public class CustomExceptionHanlder {
+public class CustomExceptionHandler {
 
     // NullPointException <- RuntimeException
     @ExceptionHandler(CustomException.class)
-    public String customException(CustomException e) {
-        return Script.back(e.getMessage());
+    public ResponseEntity<?> customException(CustomException e) {
+        String responseBody = Script.back(e.getMessage());
+        return new ResponseEntity<>(responseBody, e.getStatus());
     }
 }
